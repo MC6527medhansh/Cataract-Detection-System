d8d36b44181061a3d1726126a2347701655af54c
Medhansh Choubey <medhanshchoubey619@gmail.com>
2025-01-10 01:01:20 +0530

    updated uploadsection to use the new value

--- frontend/components/UploadSection.tsx
+++ frontend/components/UploadSection.tsx
@@ -37,10 +37,10 @@ export default function UploadSection({ setPrediction }: { setPrediction: (predi
 
     try {
       setIsUploading(true)
-      const response = await fetch('http://127.0.0.1:5001/predict', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
         method: 'POST',
         body: formData,
-      })
+      });      
 
       if (!response.ok) {
         throw new Error('Failed to fetch prediction from backend')

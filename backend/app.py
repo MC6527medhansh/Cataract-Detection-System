3c387e709ac2ecb17d0d9db2bd282f55f95eb7d9
Medhansh Choubey <medhanshchoubey619@gmail.com>
2025-01-09 23:48:33 +0530

    installed gunicorn manually and removed forced installation of flask.cors

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 import os
-os.system('pip install flask-cors')
 import sys
 import torch
 from flask import Flask, request, jsonify
@@ -32,6 +31,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
